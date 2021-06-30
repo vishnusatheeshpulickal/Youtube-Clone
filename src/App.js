7689960e6c57672feb9e15e9c01bf1b8b7f0f55a
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-06-30 20:18:58 +0530

    Sidebar is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import RecommendedVideos from './components/RecommendedVideos';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
      <Header />
+     <div className="app_page">
+       <Sidebar />
+       <RecommendedVideos />
+     </div>
     </div>
   );
 }
---
+++ src/components/RecommendedVideos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/RecommendedVideos.css'
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            <h1>RecommendedVideos</h1>
+        </div>
+    )
+}
+
+export default RecommendedVideos
