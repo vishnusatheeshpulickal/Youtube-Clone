5c900f9eadadee6877713fae615210385eb3729a
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-07-01 12:16:39 +0530

    Recommended Section is added

--- src/components/RecommendedVideos.jsx
+++ src/components/RecommendedVideos.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import '../styles/RecommendedVideos.css'
+import React from "react";
+import VideoCards from "./VideoCards";
+import "../styles/RecommendedVideos.css";
 
 function RecommendedVideos() {
-    return (
-        <div className="recommendedVideos">
-            <h1>RecommendedVideos</h1>
-        </div>
-    )
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos_video">
+        <VideoCards
+          title="June 22 2021 | Vijay Birthday | Whatsapp Status | Vijay | #shorts | BGM Master |"
+          views="100k"
+          timestamp="3 weeks ago"
+          channelImage="https://yt3.ggpht.com/ytc/AKedOLSe95IK0c_kij0nqPUXF1YP-8AWETqTreOCaxY1=s176-c-k-c0x00ffffff-no-rj"
+          channel="BGM Master"
+          image="https://i.ytimg.com/vi/SdCmfOb-ONY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDu4PblHQijScVTngFeJOVr4lzqmw"
+        />
+        <VideoCards
+          title="Samsung Odyssey G9 - 5 Things You Should Know"
+          views="30k"
+          timestamp="1 weeks ago"
+          channelImage="https://yt3.ggpht.com/ytc/AKedOLRXnymDIikEqTtRkWvV5MKrYHsQRVMipS46rUO4UQ=s88-c-k-c0x00ffffff-no-rj"
+          channel="littletofu"
+          image="https://i.ytimg.com/vi/VG98IQp7o_c/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC1xXlmBXckfaystqJk0GWyM69uWw" />
+        <VideoCards
+          title="Here’s How Your Package Ordered Online Gets to Your Doorstep | The Quint"
+          views="4M"
+          timestamp="4 Weeks ago"
+          channelImage="https://yt3.ggpht.com/ytc/AKedOLRKrecSbL351rk1UB3XT2lQcr5z9LjT0H8O91vJPw=s88-c-k-c0x00ffffff-no-rj"
+          channel="The Quint"
+          image="https://i.ytimg.com/an_webp/AnYrQt2uh_E/mqdefault_6s_480x270.webp?du=3000&sqp=CNO-9YYG&rs=AOn4CLB94xeaNyDC___M9cs7mFUA5xpaYA"
+        />
+        <VideoCards
+          title="Fluid [ Liquid ] Card in UI / Design Tutorial"
+          views="800k"
+          timestamp="5 months ago"
+          channelImage="https://yt3.ggpht.com/ytc/AKedOLQNTMLgGPhRgD-ElkhFAMuLaoqtnEQsJlrOfMsSlQ=s88-c-k-c0x00ffffff-no-rj"
+          channel="Cuberto Design"
+          image="https://i.ytimg.com/an_webp/MDRRVHPQlus/mqdefault_6s.webp?du=3000&sqp=CP-Q9YYG&rs=AOn4CLC4Ds3jLtRxgDy5tKoiZJIqQOZxKA"
+        />
+        <VideoCards
+          title="June 22 2021 | Vijay Birthday | Whatsapp Status | Vijay | #shorts | BGM Master |"
+          views="100k"
+          timestamp="3 weeks ago"
+          channelImage="https://yt3.ggpht.com/ytc/AKedOLSe95IK0c_kij0nqPUXF1YP-8AWETqTreOCaxY1=s176-c-k-c0x00ffffff-no-rj"
+          channel="BGM Master"
+          image="https://i.ytimg.com/vi/SdCmfOb-ONY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDu4PblHQijScVTngFeJOVr4lzqmw"
+        />
+        <VideoCards
+          title="June 22 2021 | Vijay Birthday | Whatsapp Status | Vijay | #shorts | BGM Master |"
+          views="100k"
+          timestamp="3 weeks ago"
+          channelImage="https://yt3.ggpht.com/ytc/AKedOLSe95IK0c_kij0nqPUXF1YP-8AWETqTreOCaxY1=s176-c-k-c0x00ffffff-no-rj"
+          channel="BGM Master"
+          image="https://i.ytimg.com/vi/SdCmfOb-ONY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDu4PblHQijScVTngFeJOVr4lzqmw"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default RecommendedVideos
+export default RecommendedVideos;
---
+++ src/components/VideoCards.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import '../styles/VideoCards.css';
+
+function VideoCards({image,title,channel,views,timestamp, channelImage}) {
+    return (
+        <div className="videoCard">
+           <img className="videoCard_thumbnail" src={image} alt="" /> 
+           <div className="videoCard_info">
+           <Avatar className="videoCard_avatar" alt={channel} src={channelImage} />
+           <div className="videoCard_text">
+               <h4>{title}</h4>
+               <p>{channel}</p>
+               <p>{views} · {timestamp}</p>
+           </div>
+           </div>
+        </div>
+    )
+}
+
+export default VideoCards
